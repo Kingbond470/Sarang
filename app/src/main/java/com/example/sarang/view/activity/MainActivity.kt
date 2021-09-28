2d025a1602ff127bab0b00b65c45d9bbb3a54a03
Kingbond470 <mausamsingh470@gmail.com>
2021-09-28 19:29:00 +0530

    Material UI Addition

--- app/src/main/java/com/example/sarang/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/sarang/view/activity/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.sarang.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import com.example.sarang.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get input text
+        val inputText = outlinedTextField.editText?.text.toString()
+
+        outlinedTextField.editText?.doOnTextChanged { inputText, _, _, _ ->
+            // Respond to input text change
+        }
     }
 }
