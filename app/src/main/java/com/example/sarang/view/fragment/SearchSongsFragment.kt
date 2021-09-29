3ac8187dcae96c8884cd41d9f9f9d9f48b48f410
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-09-29 09:10:17 +0530

    Detail Search Fragment Added | Sarang

--- app/src/main/java/com/example/sarang/view/fragment/SearchSongsFragment.kt
+++ app/src/main/java/com/example/sarang/view/fragment/SearchSongsFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.sarang.R
 
-class SearchFragment : Fragment() {
+class SearchSongsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_search_songs, container, false)
     }
 
 }
