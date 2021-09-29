0cbaa0863ffcf9d85991d14e89d523e365a2fafb
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-09-29 10:14:57 +0530

    Detail Search Fragment Added | Sarang

--- app/src/main/java/com/example/sarang/view/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/sarang/view/fragment/SearchFragment.kt
@@ -1,11 +1,15 @@
 package com.example.sarang.view.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.example.sarang.R
+import kotlinx.android.synthetic.main.fragment_search.*
+
 
 class SearchFragment : Fragment() {
 
@@ -14,7 +18,18 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(com.example.sarang.R.layout.fragment_search, container, false)
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cardViewSearchBar.setOnClickListener(View.OnClickListener {
+            val searchSongsFragment = SearchSongsFragment()
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.mainActivity, searchSongsFragment)
+            transaction.commit()
+        })
     }
 
 }
