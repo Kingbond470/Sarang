83794e018293c7a4a1c47760082168a44bfec1d8
Kingbond470 <mausamsingh470@gmail.com>
2021-09-29 19:02:50 +0530

    Notification Fragment Layout

--- app/src/main/java/com/example/sarang/view/fragment/NotificationFragment.kt
+++ app/src/main/java/com/example/sarang/view/fragment/NotificationFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.example.sarang.R
+import kotlinx.android.synthetic.main.fragment_notification.*
 
 
 class NotificationFragment : Fragment() {
@@ -16,6 +18,17 @@ class NotificationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ivBackNotification.setOnClickListener {
+            val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+            ft.replace(com.example.sarang.R.id.framelayout_container, HomeFragment(), "Home Fragment")
+            ft.addToBackStack(null)
+            ft.commit()
+        }
     }
 
 
