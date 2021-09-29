91432560afdbddf5fa5303adda855941f8278fd4
Kingbond470 <mausamsingh470@gmail.com>
2021-09-29 09:32:04 +0530

    Adding Additional Fragment

--- app/src/main/java/com/example/sarang/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/sarang/view/fragment/HomeFragment.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.sarang.R
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_home.*
+import androidx.fragment.app.FragmentTransaction
 
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(com.example.sarang.R.layout.fragment_home, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ivNotification.setOnClickListener {
+
+//            val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+//            ft.replace(com.example.sarang.R.id.framelayout_container, HomeFragment(), "Home Fragment")
+//            ft.addToBackStack(null)
+//            ft.commit()
+            Toast.makeText(context,"Notification",Toast.LENGTH_SHORT).show()
+        }
+
+
+        ivRecentlyPlayed.setOnClickListener {
+            Toast.makeText(context,"Recently PLayed",Toast.LENGTH_SHORT).show()
+        }
+
+        ivSettings.setOnClickListener {
+            Toast.makeText(context,"Settings",Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
