0867eb2fd2480773e1e77e450b2249d57ec600d3
Prabhakar <prabhakarkr802@gmail.com>
2021-09-30 07:39:20 +0530

    Premium Layout Fix Issue

--- app/src/main/java/com/example/sarang/view/fragment/LibraryFragment.kt
+++ app/src/main/java/com/example/sarang/view/fragment/LibraryFragment.kt
@@ -1,23 +1,17 @@
 package com.example.sarang.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.sarang.R
 
 
-class LibraryFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+class LibraryFragment : Fragment(R.layout.fragment_library) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
