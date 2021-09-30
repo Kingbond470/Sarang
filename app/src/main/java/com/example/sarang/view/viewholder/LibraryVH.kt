a354c1a2f3a6c904dfacc7af53f2290a802f0ae0
Prabhakar <prabhakarkr802@gmail.com>
2021-09-30 12:42:36 +0530

    Library layout Complete

---
+++ app/src/main/java/com/example/sarang/view/viewholder/LibraryVH.kt
@@ -0,0 +1,16 @@
+package com.example.sarang.view.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.sarang.view.model.LibraryModel
+import kotlinx.android.synthetic.main.library_linear_layout.view.*
+
+class LibraryVH(val view: View) : RecyclerView.ViewHolder(view) {
+    fun setLibraryData(model: LibraryModel) {
+        view.apply {
+            singer_name.text = model.singerName
+            Glide.with(singer_img).load(model.singerImage).into(singer_img)
+        }
+    }
+}
