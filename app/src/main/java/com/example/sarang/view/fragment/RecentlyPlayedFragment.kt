a260c49a5224cf944ddd5ee99931e15dda116ae8
Kingbond470 <mausamsingh470@gmail.com>
2021-10-03 21:20:37 +0530

    Recently Fragment Fixed

--- app/src/main/java/com/example/sarang/view/fragment/RecentlyPlayedFragment.kt
+++ app/src/main/java/com/example/sarang/view/fragment/RecentlyPlayedFragment.kt
@@ -9,10 +9,11 @@ import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.sarang.R
 import com.example.sarang.view.adapter.ToGetYouStartedAdapter
+import com.example.sarang.view.itunes.ToGetYouStartedClickListener
 import com.example.sarang.view.model.ToGetYouStarted
 import kotlinx.android.synthetic.main.fragment_recently_played.*
 
-class RecentlyPlayedFragment : Fragment() {
+class RecentlyPlayedFragment : Fragment(), ToGetYouStartedClickListener{
 
     private val togetyoustartedList = ArrayList<ToGetYouStarted>()
     private val togetYouStartedYesterday=ArrayList<ToGetYouStarted>()
@@ -30,22 +31,61 @@ class RecentlyPlayedFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         ivBackRecentlyPlayed.setOnClickListener {
             val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
-            ft.replace(com.example.sarang.R.id.framelayout_container, HomeFragment(), "Home Fragment")
+            ft.replace(R.id.framelayout_container, HomeFragment(), "Home Fragment")
             ft.addToBackStack(null)
             ft.commit()
         }
 
         //for to get you started
+        togetyoustartedList.clear()
         for(i in 0..10){
-            togetyoustartedList.add(ToGetYouStarted(R.drawable.play_date,"Songs"))
+            togetyoustartedList.add(ToGetYouStarted(R.drawable.togetyoustarted_youme, "You Me"))
+            togetyoustartedList.add(
+                ToGetYouStarted(
+                    R.drawable.togetyoustarted_moodbooster,
+                    "Mood Booster"
+                )
+            )
+            togetyoustartedList.add(
+                ToGetYouStarted(
+                    R.drawable.togetyoustarted_feelingmyself,
+                    "Feeling Myself"
+                )
+            )
+            togetyoustartedList.add(
+                ToGetYouStarted(
+                    R.drawable.togetyoustarted_feelinggood,
+                    "Feeling Good"
+                )
+            )
+            togetyoustartedList.add(
+                ToGetYouStarted(
+                    R.drawable.togetyoustarted_darkstormy,
+                    "Dark Stormy"
+                )
+            )
+            togetyoustartedList.add(
+                ToGetYouStarted(
+                    R.drawable.togetyoustarted_chilltracks,
+                    "Chill Tracks"
+                )
+            )
+            togetyoustartedList.add(
+                ToGetYouStarted(
+                    R.drawable.togetyoustarted_feelinggoodpiano,
+                    "Piano"
+                )
+            )
         }
 
-//        val toGetYouStartedAdapter = ToGetYouStartedAdapter(togetyoustartedList, this)
-//        val gridLayoutManager =
-//            GridLayoutManager(context, 1, GridLayoutManager.HORIZONTAL, false)
-//        recyclerViewRecentlyPlayedToday.adapter = toGetYouStartedAdapter
-//        recyclerViewRecentlyPlayedToday.layoutManager = gridLayoutManager
-//        recyclerViewRecentlyPlayedToday.hasFixedSize()
+        togetyoustartedList.shuffle()
+
+        val toGetYouStartedAdapter = ToGetYouStartedAdapter(togetyoustartedList, this)
+        val gridLayoutManager =
+            GridLayoutManager(context, 1, GridLayoutManager.HORIZONTAL, false)
+        recyclerViewRecentlyPlayedToday.adapter = toGetYouStartedAdapter
+        recyclerViewRecentlyPlayedToday.layoutManager = gridLayoutManager
+        recyclerViewRecentlyPlayedToday.hasFixedSize()
 
         // for to get you started yesterday
         for(i in 0..10){
@@ -79,6 +119,27 @@ class RecentlyPlayedFragment : Fragment() {
 //        recyclerViewDateRecentlyPlayedToday.adapter = toGetYouStartedAdapter
 //        recyclerViewDateRecentlyPlayedToday.layoutManager = gridLayoutManagerHospitals
 //        recyclerViewDateRecentlyPlayedToday.hasFixedSize()
+
+    }
+
+    override fun onMoodClick(position: Int, mood: ToGetYouStarted) {
+
+        val args = Bundle()
+        args.putString("artistName", mood.title)
+        args.putString("artistImage", mood.albumImage.toString())
+
+        val artistFragment = ArtistFragment()
+        artistFragment.arguments = args
+
+        val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+
+        ft.replace(
+            R.id.framelayout_container,
+            artistFragment,
+            "Fragment"
+        )
+        ft.addToBackStack(null)
+        ft.commit()
 
     }
 
